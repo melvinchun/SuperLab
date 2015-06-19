c22fc756de630c2dd67af75c059701e30ee79899
Melvin Portillo <melvin.manuel.portillo@gmail.com>
2015-06-19 16:05:38 -0600

    :/

--- Investigador.cpp
+++ Investigador.cpp
@@ -6,13 +6,13 @@
 using std::string;
 using std::stringstream;
 
-Investigador::Investigador(int atendidos, int cerrados, int sin_resolver, string nombre, string usuario, string contraseña, int edad,  string id, string fecha_nacimiento):Persona(nombre, usuario, contraseña, edad, id, fecha_nacimiento){
+Investigador::Investigador(int atendidos, int cerrados, int sin_resolver, string nombre, string usuario, string contraseña,  string id, string fecha_nacimiento, int edad):Persona (nombre, usuario, contraseña, id, fecha_nacimiento, edad){
 	this->atendidos = atendidos;
 	this->cerrados = cerrados;
 	this->sin_resolver = sin_resolver;
 }
 
-Investigador::Invesigador(const Investigador& other):Persona(other){
+Investigador::Investigador(const Investigador& other):Persona(other){
 	atendidos = other.atendidos;
 	cerrados = other.cerrados;
 	sin_resolver = other.sin_resolver;
@@ -24,29 +24,22 @@ void Investigador::setAtendidos(int atendidos){
 	this->atendidos = atendidos;
 }
 
-int Investigador::getAtendidos(){
+int Investigador::getAtendidos() const{
 	return this->atendidos;
 }
 
-void Investigador::setCerrados(){
+void Investigador::setCerrados(int cerrados){
 	this->cerrados = cerrados;
 }
 
-int Investigador::getCerrados(){
-	return this->cerrados;
+int Investigador::getCerrados() const{
+	return cerrados;
 }
 
-void Investigador::setSin_resolver(){
+void Investigador::setSin_resolver(int sin_resolver){
 	this->sin_resolver = sin_resolver;
 }
 
-int Investigador::getSin_resolver(){
+int Investigador::getSin_resolver() const{
 	return this->sin_resolver;
-}
-
-string toString() const{
-	stringstream ss;
-	ss << " Investigador: Numeros de Casos Atendidos: " << atendidos << " Numero de Casos Cerrados: " << cerrados << " Numero de Casos Sin Resolver: " << sin_resolver << Persona::toString();
-
-	return ss.str();
 }
--- Investigador.hpp
+++ Investigador.hpp
@@ -5,16 +5,15 @@
 using std::string;
 
 class Investigador:public Persona{
-	int atendidos, cerrados, sin_resorlver;
+	int atendidos, cerrados, sin_resolver;
 public:
 	Investigador(int, int, int, string, string, string, string, string, int);
 	Investigador(const Investigador&);
 	~Investigador();
-	string toString()const;
 	void setAtendidos(int atendidos);
-	int getAtendidos();
+	int getAtendidos()const;
 	void setCerrados(int cerrados);
-	int getCerrados()const;
+	int getCerrados() const;
 	void setSin_resolver(int sin_resolver);
-	int getSin_resolver()const;
+	int getSin_resolver() const;
 };
